c585c04a0ba815efa75bfd80c86a634416c1bd34
sabatex <serhiy.lakas@hotmail.com>
2024-09-11 15:54:59 +0300

    add testing,demo and nodify SabatexEnumDropDown

---
+++ Sabatex.RadzenBlazor.Demo/Models/DaysOfWeek.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sabatex.RadzenBlazor.Demo.Models;
+
+public enum DaysOfWeek
+{
+    [Display(Description = "Sunday")]
+    Sunday = 0,
+
+    [Display(Description = "Monday")]
+    Monday = 1,
+
+    [Display(Description = "Tuesday")]
+    Tuesday = 2,
+
+    [Display(Description = "Wednesday")]
+    Wednesday = 3,
+
+    [Display(Description = "Thursday")]
+    Thursday = 4,
+
+    [Display(Description = "Friday")]
+    Friday = 5,
+
+    [Display(Description = "Saturday")]
+    Saturday = 6
+}
