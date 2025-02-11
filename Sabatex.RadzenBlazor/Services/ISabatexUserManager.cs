572916e213c2ebfd98b64863a0c849d760bc1a25
sabatex <serhiy.lakas@hotmail.com>
2025-02-11 20:51:57 +0200

    *

---
+++ Sabatex.RadzenBlazor/Services/ISabatexUserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabatex.RadzenBlazor.Services;
+
+public interface ISabatexUserManager
+{
+    /// <summary>
+    /// Read all roles in application
+    /// </summary>
+    /// <returns></returns>
+    IEnumerable<string> GetAvaliableRoles();
+}
